7f22287762e1a7f3626aa3860b78f5f96c298027
Hunter37594 <hunter37594@gmail.com>
2025-02-23 09:26:54 -0600

    Add constant fields

--- soba_app/lib/main.dart
+++ soba_app/lib/main.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:soba_app/login.dart';
+import 'package:soba_app/navigation.dart';
+import 'package:soba_app/button.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
+
+const double dfInsets = 20; // Default insets for padding & margin
+const double dfRadius = 20; // Default border radius
+const Color yellow = Color(0xFFFFDD85), coral = Color(0xFFFF6F61); // Theme colors
+const LinearGradient gradientOrange = LinearGradient(
+  begin: Alignment.centerLeft,
+  end: Alignment.centerRight,
+  colors: [
+    yellow,
+    coral,
+  ],
+); // Theme gradient
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.orange,
-        body: LoginPage(),
+        backgroundColor: coral,
+        // body: ButtonPage(),
+        body: MapPage(),
       ),
+      theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: coral, primary: coral, secondary: yellow, dynamicSchemeVariant: DynamicSchemeVariant.fidelity),
+        ),
     );
   }
 }
