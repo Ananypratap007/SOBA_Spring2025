f12b175513f6deaace0f122e60772ab21c1c7498
Hunter37594 <hunter37594@gmail.com>
2025-02-23 09:25:55 -0600

    Implement navigation.dart

--- soba_app/lib/navigation.dart
+++ soba_app/lib/navigation.dart
@@ -1,100 +1,94 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:soba_app/main.dart' hide MainApp;
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
-void main() {
-  runApp(MyApp());
+class MapPage extends StatefulWidget {
+  const MapPage({super.key});
+  
+  @override
+  State<MapPage> createState() => _MapPageState();
 }
 
-class MyApp extends StatelessWidget {
+class _MapPageState extends State<MapPage> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-    );
-  }
-}
+    final scheme = Theme.of(context).colorScheme;
 
-class HomeScreen extends StatefulWidget {
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int _selectedIndex = 1;
-  late GoogleMapController mapController;
-
-  final LatLng _center = const LatLng(33.5186, -86.8104);
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          GoogleMap(
-            onMapCreated: _onMapCreated,
-            initialCameraPosition: CameraPosition(
-              target: _center,
-              zoom: 14.0,
-            ),
-          ),
-          Positioned(
-            top: 40,
-            left: 10,
-            right: 10,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 16.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black26,
-                    blurRadius: 5.0,
-                  ),
-                ],
-              ),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Search here...',
-                  border: InputBorder.none,
-                  icon: Icon(Icons.search, color: Colors.orange),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
       bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.map),
-            label: 'Map',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.orange,
+        backgroundColor: Colors.white,
+        selectedItemColor: scheme.primary,
         unselectedItemColor: Colors.grey,
-        onTap: _onItemTapped,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.place), label: 'Map'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+        currentIndex: 1,
+      ),
+      body: SlidingUpPanel(
+        minHeight: 50,
+        maxHeight: 450,
+        borderRadius: BorderRadius.vertical(top: Radius.circular(dfRadius)),
+        panel: Column(
+            children: [
+              // None :(
+            ],
+          ),
+        body: Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.fromLTRB(dfInsets, 2.5*dfInsets, dfInsets, dfInsets),
+              decoration: BoxDecoration(
+                gradient: gradientOrange,
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.arrow_back, color: Colors.white, size: 40), // TODO: Add functionality
+                  Expanded(child: SizedBox.shrink()),
+                  _SearchBar(scheme.primary),
+                ],
+              )
+            ),
+            Image.asset('assets/maps_placeholder.png', scale: 0.945),
+          ],
+        ),
       ),
     );
   }
+}
+
+class _SearchBar extends StatelessWidget {
+  final grey = const Color(0xFFF2F2F7);
+  final Color color;
+  _SearchBar(this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 0.75*MediaQuery.of(context).size.width,
+      child: TextFormField(
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
+          focusColor: color,
+          border: _border(grey),
+          enabledBorder: _border(grey),
+          hintText: 'Search here...',
+          contentPadding: const EdgeInsets.symmetric(vertical: dfInsets/2),
+          prefixIcon: const Icon(
+            Icons.search,
+            color: Colors.grey,
+          ),
+        ),
+        onFieldSubmitted: (value) {},
+      ),
+    );
+  }
+
+  OutlineInputBorder _border(Color color) => OutlineInputBorder(
+        borderSide: BorderSide(width: 0.5, color: color),
+        borderRadius: BorderRadius.circular(12),
+      );
 }
