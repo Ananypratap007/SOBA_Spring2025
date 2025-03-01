d2cdec150ca32178a9974568aca1a3fece8905c7
Danielle Cox <imafigureskater@gmail.com>
2025-03-01 11:40:55 -0500

    Added redirect for login

--- soba_app/lib/domain/routes_and_state.dart
+++ soba_app/lib/domain/routes_and_state.dart
@@ -3,7 +3,10 @@ import 'package:go_router/go_router.dart';
 import 'package:soba_app/bottom_nav_screen.dart';
 import 'package:soba_app/checkin/checkin_screen.dart';
 import 'package:soba_app/home/home_screen.dart';
+import 'package:soba_app/login.dart';
 import 'package:soba_app/profile/profile_screen.dart';
+
+bool _isLoggedIn = false;
 
 final GoRouter router = GoRouter(
   routes: <RouteBase>[
@@ -35,10 +38,21 @@ final GoRouter router = GoRouter(
             return CheckinScreen();
           },
         ),
+        GoRoute(
+          path: 'login',
+          builder: (BuildContext context, GoRouterState state) {
+            return LoginPage(
+              onSuccessfulLogin: () {
+                _isLoggedIn = true;
+                context.go('/');
+              },
+            );
+          },
+        ),
       ],
     ),
   ],
   // This is where we will handle the login - check if the user is logged in
   // and redirect if not.
-  redirect: (state, goRouterState) => null,
+  redirect: (state, goRouterState) => !_isLoggedIn ? '/login' : null,
 );
