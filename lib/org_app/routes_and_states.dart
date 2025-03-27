55a854f4735a5e40b55cec33e52ae9d426cefbbc
Hunter37594 <hunter37594@gmail.com>
2025-03-27 14:39:48 -0500

    Create tiles page

--- lib/org_app/routes_and_states.dart
+++ lib/org_app/routes_and_states.dart
@@ -1,6 +1,7 @@
 import 'package:go_router/go_router.dart';
 import 'checkintimer.dart';
 import 'profile.dart';
+import 'tiles.dart';
 
 final GoRouter router = GoRouter(routes: [
   GoRoute(
@@ -13,5 +14,9 @@ final GoRouter router = GoRouter(routes: [
         final initialDuration =
             state.extra as Duration? ?? Duration(minutes: 10);
         return CheckInTimer(initialDuration: initialDuration);
-      })
+      }),
+  GoRoute(
+    path: '/tiles',
+    builder: (context, state) => TilesPage(),
+  )
 ]);
