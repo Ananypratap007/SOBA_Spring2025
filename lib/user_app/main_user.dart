038510011a75d42fd6fc0dec9897795df5d6a379
Daniel <olekabrida@gmail.com>
2025-04-11 16:33:20 -0500

    Configure Settings to work with web

--- lib/user_app/main_user.dart
+++ lib/user_app/main_user.dart
@@ -1,12 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:soba_app/config/firebase_config.dart';
+import 'package:soba_app/config/firebase_options.dart';
 import 'domain/routes_and_state.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Initialize Firebase
-  await FirebaseConfig.init();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   // Wait for the initial auth state to be determined
   await Future.delayed(const Duration(milliseconds: 500));
